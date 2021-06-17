5ecd5e0d639b15c40f675cba246672e24b6333c5
root <allanmasiga032@mail.com>
2021-06-17 12:52:30 -0700

    Add extra files in root directory

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+/* This is tthhee main entry point of the proggrrum and it puts out a simple statement */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle\n");
+i	return(0);
+}
