2a04e10ad420a66d866304c3c987aad2ea2544a7
root <allanmasiga032@mail.com>
2021-06-19 06:38:58 -0700

    Correct Error

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -21,15 +21,15 @@ int main(void)
 
 	if (lastDigit > 5)
 	{
-		printf ("Last digit of %d is %d and is  greater than 5\n", n,lastDigit);
+		printf ("Last digit of %d is %d and is  greater than 5", n,lastDigit);
 	}
 	else if (lastDigit == 0)
 	{
-		printf ("Last digit of %d is %d and is zero\n", n, lastDigit);
+		printf ("Last digit of %d is %d and is 0", n, lastDigit);
 	}
 	else if (lastDigit != 0 && lastDigit < 6)
 	{
-		printf ("Last digit of %d is %d and is less than 6 and not zero\n", n, lastDigit);
+		printf ("Last digit of %d is %d and is less than 6 and not 0", n, lastDigit);
 	};
 	return (0);
 }
