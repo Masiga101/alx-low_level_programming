3190acb76f81fb02074dafa0f0e515d188f6a057
root <allanmasiga032@mail.com>
2021-06-19 07:43:17 -0700

    Correct some errors

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+/* more headers goes there */
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char cha;
+
+		
+	for (cha = 'a'; cha <= 'z'; cha++)
+	{
+		putchar(cha);
+	}
+
+	for (cha = 'A'; cha <= 'Z'; cha++)
+	{
+		putchar(cha);
+	}
+	putchar('\n');
+	
+	return (0);
+}
