488866d4509987d4722f70f9b1e30009e7ca17bd
root <allanmasiga032@mail.com>
2021-06-19 07:00:16 -0700

    Correct Error

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+/* more headers goes there */
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int n;
+	char cha;
+
+		
+	for (ch = 'a'; ch <= 'z'; ch++)
+	{
+		putchar("%c\n", cha);
+	}
+	
+	return (0);
+}
