c7eb4393f000db5b037c5b4000b5d74728880c24
root <allanmasiga032@mail.com>
2021-06-19 08:06:00 -0700

    Add extra c files

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+/* more headers goes there */
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int i;
+
+		
+	for (i = 0; i < 10; i++)
+	{
+		putchar(i);
+	}
+
+	putchar('\n');
+	
+	return (0);
+}
