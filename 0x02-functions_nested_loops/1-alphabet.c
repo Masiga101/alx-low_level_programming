92e5c20567a23944cca8ca865a40eea82fe611d3
root <allanmasiga032@mail.com>
2021-07-01 08:36:44 -0700

    Add file 1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+
+/**
+ * print_alphabet- Entry point
+ * print_alphabet- print lowercase alphabets
+ * _putchar: Prints character
+ * return: 0
+ */
+
+void print_alphabet(void)
+{
+	char x = 'a';
+	while (x <= 'z')
+	{
+		_putchar(x);
+		x++;
+	}
+	_putchar('\n');
+}
