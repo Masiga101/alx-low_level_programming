0d70c855a6ec7a5728fedbdc26b3875af01c253c
root <allanmasiga032@mail.com>
2021-07-02 03:11:39 -0700

    Add files

---
+++ 0x05-pointers_arrays_strings/holberton.h
@@ -0,0 +1,35 @@
+/*update the value of an int pointer to 98*/
+void reset_to_98(int *n);
+
+/*Swaps two integers*/
+void swap_int(int *a, int *b);
+
+/*Returns the length of a string*/
+int _strlen(char *s);
+
+/*Print a string to stdout followed by a new line*/
+void _puts(char *str);
+
+/*Prints a reversed string*/
+void print_rev(char *s);
+
+/*Reverses a string*/
+void rev_string(char *s);
+
+/*Prints characters of a string followed by a new line*/
+void puts2(char *str);
+
+/*print half a string*/
+void puts_half(char *str);
+
+/*Prints n elements of an array of integers*/
+void print_array(int *a, int n);
+
+/*Copy string pointed to by src*/
+char *_strcpy(char *dest, char *str);
+
+/*prints characters*/
+int _putchar(char c);
+
+/*Converts a string to an integer*/
+int _atoi(char *s);
